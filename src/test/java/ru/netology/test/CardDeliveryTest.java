283ca33ab489315ed4087ef06fa129b98ca33b09
Yudin Egor <yudin.egor26@gmail.com>
2021-10-05 13:30:40 +0500

    add Data file

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,64 +1,66 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
-import com.github.javafaker.Faker;
+import lombok.val;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
+import ru.netology.data.DataGenerator;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.By.cssSelector;
+import static org.openqa.selenium.Keys.DELETE;
+import static ru.netology.data.DataGenerator.generateDate;
 
 public class CardDeliveryTest {
-    private Faker faker;
 
-    String dateFormation(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
+    private final String date = generateDate(3);
+    private final String anotherDate = generateDate(4);
+
 
     @BeforeEach
-    void setUpAll() {
-        faker =new Faker(new Locale("ru"));
+    void setup() {
+        open("http://localhost:9999");
+        $(cssSelector("[data-test-id=date] .input__control")).doubleClick().sendKeys(DELETE);
+    }
+
+    @AfterEach
+    void tearDown() {
+        closeWindow();
     }
 
     @Test
-    void shouldGenerateTestData(){
-
-        RegistrationCard info = DataGenerator
-                .Registration
-                .generateByCard("ru");
-        System.out.println(info.getCity() + "\n" + info.getMeetingDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + "\n" + info.getName() + "\n" + info.getPhone());
-
-        String dateMeeting = info.getMeetingDate().format((DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-
-        open("http://localhost:9999");
-        $("[placeholder='Город']").setValue(info.getCity());
-        $("[data-test-id='date'] [type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [type='tel']").setValue(dateMeeting);
-        $("[name='name']").setValue(info.getName());
-        $("[name='phone']").setValue(info.getPhone());
-        $("[data-test-id='agreement']").click();
-        $(byText("Запланировать")).click();
-        $(".notification__title").shouldHave(exactText("Успешно!"));
-        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + dateMeeting));
-        $("[data-test-id='success-notification'] .icon-button__content").click();
-
-
-        String dateMeeting2 = info.getMeetingDate().plusDays(1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id='date'] [type='tel']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [type='tel']").setValue(dateMeeting2);
-        $(byText("Запланировать")).click();
-        $("[data-test-id='replan-notification'] .notification__title").shouldHave(exactText("Необходимо подтверждение"));
-        $(byText("Перепланировать")).click();
-        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + dateMeeting2));
-
+    void shouldTestOrderWithValidData() {
+        val validUser = DataGenerator.Registration.generateUser("ru");
+        $(cssSelector("[data-test-id=city] .input__control")).setValue(validUser.getCity());
+        $(cssSelector("[data-test-id=date] .input__control")).sendKeys(date);
+        $(cssSelector("[data-test-id=name] .input__control")).setValue(validUser.getName());
+        $(cssSelector("[data-test-id=phone] .input__control")).setValue(validUser.getPhone());
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $(".notification__content").waitUntil(Condition.visible,
+                1500).shouldHave(exactText("Встреча успешно запланирована на " + date));
     }
-}
+
+    @Test
+    void shouldTestOrderWithValidDataAndAnotherMeetingDate() {
+        val validUser = DataGenerator.Registration.generateUser("ru");
+        $(cssSelector("[data-test-id=city] .input__control")).setValue(validUser.getCity());
+        $(cssSelector("[data-test-id=date] .input__control")).sendKeys(date);
+        $(cssSelector("[data-test-id=name] .input__control")).setValue(validUser.getName());
+        $(cssSelector("[data-test-id=phone] .input__control")).setValue(validUser.getPhone());
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $(".notification__content").waitUntil(Condition.visible,
+                1500).shouldHave(exactText("Встреча успешно запланирована на " + date));
+        $(cssSelector("[data-test-id=date] .input__control")).doubleClick().sendKeys(DELETE, anotherDate);
+        $$("button").find(Condition.exactText("Запланировать")).click();
+        $$(".button__text").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification]").waitUntil(Condition.visible,
+                1500).shouldHave(exactText("Успешно! Встреча успешно запланирована на " + anotherDate));
+    }
+
+} 
